92072672b11e034bcf5854a62fc90f2be3458e10
daniel <exodaniel@gmail.com>
2020-08-11 14:16:46 +0200

    improve test scenario

--- test/reproduce_a_scenario.py
+++ test/reproduce_a_scenario.py
@@ -1,15 +1,19 @@
 import rvo2
 import numpy as np
 
-def reproduce_test_scenario():
+def reproduce_test_scenario(plotting=False):
     sim = rvo2.PyRVOSimulator(1/60., 1.5, 5, 1.5, 2, 0.4, 2)
 
-    a0 = sim.addAgent((0, 0))
-    a1 = sim.addAgent((1, 0))
+    a0 = sim.addAgent((-1, -1))
+    a1 = sim.addAgent((1, -1))
     a2 = sim.addAgent((1, 1))
-    a3 = sim.addAgent((0, 1), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+    a3 = sim.addAgent((-1, 1), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
 
-    sim.addObstacle([(0.1, 0.1), (-0.1, 0.1), (-0.1, -0.1)])
+    obstacles = [
+        [(0.3, 0.1), (0.1, 0.1), (0.1, -0.1)]
+    ]
+    for obstacle in obstacles:
+        sim.addObstacle(obstacle)
     sim.processObstacles()
 
     sim.setAgentPrefVelocity(a0, (1, 1))
@@ -20,18 +24,25 @@ def reproduce_test_scenario():
     print('Running simulation')
 
     positions_log = []
-    for step in range(20):
+    for step in range(200):
         sim.doStep()
 
         positions = [sim.getAgentPosition(agent_no) for agent_no in (a0, a1, a2, a3)]
         positions_log.append(positions)
 
     positions_log = np.array(positions_log)
-    # np.savez("positions_log_for_test_scenario.npz", positions_log=positions_log)
+#     np.savez("positions_log_for_test_scenario.npz",
+#              positions_log=positions_log,
+#              obstacles=np.array(obstacles))
 
     archive = np.load("positions_log_for_test_scenario.npz")
 
     test_passed = np.allclose(archive['positions_log'], positions_log)
+
+    if plotting:
+        plot_obstacles(np.array(obstacles))
+        plot_positions_log(positions_log)
+        plot_positions_log(archive['positions_log'])
 
     if test_passed:
         print("Test passed!")
@@ -39,6 +50,18 @@ def reproduce_test_scenario():
         print("Test failed!")
     return test_passed
 
+def plot_positions_log(positions_log):
+    n_traj = positions_log.shape[1]
+    for i in range(n_traj):
+        plt.plot(positions_log[:,i,0], positions_log[:,i,1])
+
+def plot_obstacles(obstacles):
+    for obstacle in obstacles:
+        c = np.concatenate([obstacle, obstacle[:1]], axis=0)
+        plt.plot(c[:,0], c[:,1], 'k')
+
 
 if __name__ == "__main__":
-    reproduce_test_scenario()
+    from matplotlib import pyplot as plt
+    reproduce_test_scenario(plotting=True)
+    plt.show()
